418556f7085984f98079d37bfcd882f3927c99e1
nighostchris <chrisliupascal@gmail.com>
2020-07-01 16:54:15 +0800

    Added CustomDivider & 2 new buttons for social platform login

--- secretchat/LoginPageView.swift
+++ secretchat/LoginPageView.swift
@@ -14,27 +14,71 @@ struct LoginPageView: View {
             Color("BackgroundColor")
                 .edgesIgnoringSafeArea(.all)
 
-            VStack(spacing: 15) {
-                CustomInputField(title: "Email Address")
-                
-                CustomInputField(title: "Password")
-                
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                    HStack {
-                        Spacer()
-                        Text("Login")
-                            .foregroundColor(Color.black)
-                            .fontWeight(.bold)
-                        Spacer()
+            VStack(spacing: 0) {
+                VStack(spacing: 15) {
+                    CustomInputField(title: "Email Address")
+                    
+                    CustomInputField(title: "Password")
+                    
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        HStack {
+                            Spacer()
+                            Text("Login")
+                                .foregroundColor(Color.black)
+                                .fontWeight(.bold)
+                            Spacer()
+                        }
+                        .frame(height: 40)
+                        .background(Color("LoginButtonColor"))
+                        .cornerRadius(10)
                     }
-                    .frame(height: 40)
-                    .background(Color("LoginButtonColor"))
-                    .cornerRadius(10)
+                    .padding(.horizontal)
+                    .padding(.top, 10)
                 }
                 .padding(.horizontal)
-                .padding(.top, 10)
+                
+                CustomDivider(label: "Or", verticalPadding: 36)
+                    .padding(.horizontal)
+                
+                VStack(spacing: 24) {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        HStack {
+                            Spacer()
+                            Image("facebook-logo")
+                                .renderingMode(.original)
+                                .resizable()
+                                .scaledToFit()
+                            Text("Continue with Facebook")
+                                .foregroundColor(Color("ButtonTextColor"))
+                                .fontWeight(.bold)
+                            Spacer()
+                        }
+                        .frame(height: 40)
+                        .background(Color("FacebookLoginButtonColor"))
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                    }
+                    
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        HStack {
+                            Spacer()
+                            Image("google-logo")
+                                .renderingMode(.original)
+                                .resizable()
+                                .scaledToFit()
+                            Text("Continue with Google")
+                                .foregroundColor(Color.black)
+                                .fontWeight(.bold)
+                            Spacer()
+                        }
+                        .frame(height: 40)
+                        .background(Color("GoogleLoginButtonColor"))
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                    }
+                }
+                .padding(.horizontal)
             }
-            .padding(.horizontal)
         }
     }
 }
@@ -42,7 +86,6 @@ struct LoginPageView: View {
 struct LoginPageView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            LoginPageView().colorScheme(.light)
             LoginPageView().colorScheme(.dark)
         }
         .previewDevice(PreviewDevice(rawValue: "iPhone XS"))
---
+++ secretchat/Models/CustomDivider.swift
@@ -0,0 +1,46 @@
+//
+//  CustomDivider.swift
+//  secretchat
+//
+//  Created by Chris Liu on 1/7/2020.
+//  Copyright © 2020 Secret Chat. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct CustomDivider: View {
+    let label: String
+    let verticalPadding: CGFloat
+    
+    init(label: String, verticalPadding: CGFloat) {
+        self.label = label
+        self.verticalPadding = verticalPadding
+    }
+    
+    var line: some View {
+        VStack {
+            Divider()
+                .frame(height: 2)
+                .background(Color("DividerColor"))
+        }
+        .padding(.horizontal)
+    }
+
+    var body: some View {
+        HStack {
+            line
+            Text(label)
+                .foregroundColor(Color("DividerColor"))
+                .font(.system(size: 14))
+            line
+        }
+        .padding(.vertical, verticalPadding)
+    }
+}
+
+struct CustomDivider_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomDivider(label: "or", verticalPadding: 36)
+    }
+}
